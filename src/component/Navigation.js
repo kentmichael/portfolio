81cf3d69130a57e16bcaeb059dfdecd5067afa58
Kent Michael San <kentmichaelsan22@gmail.com>
2022-06-11 20:02:53 +0800

    Fix logo onclick

--- src/component/Navigation.js
+++ src/component/Navigation.js
@@ -1,13 +1,22 @@
 import React from "react"
+import { useNavigate } from "react-router-dom"
 import { Nav, Div, Button, Button1, Span } from "./styles/Navigation/Nav"
 
 const Header = (props) => {
   const { darkMode, setDarkMode } = props.mode
+  const navigate = useNavigate()
 
   return (
     <Nav>
       <Div>
-        <Button>KS.</Button>
+        <Button
+          onClick={() => {
+            props.setActiveLink("about-me")
+            navigate("portfolio")
+          }}
+        >
+          KS.
+        </Button>
         <Button1 onClick={() => setDarkMode(!darkMode)}>
           <Span
             className={darkMode ? "fa-solid fa-sun" : "fa-solid fa-moon"}
